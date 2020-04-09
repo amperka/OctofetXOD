6b1411d9db3d3d27383e963bedbd0baee957da6d
Igor89 <igor@amperka.ru>
2020-04-10 01:08:20 +0300

    add terminal node dev' and fix descriptions for nodes

--- write-byte/patch.cpp
+++ write-byte/patch.cpp
@@ -1,12 +1,15 @@
-
 struct State {};
 
 {{ GENERATED_CODE }}
 
 void evaluate(Context ctx) {
+    auto dev = getValue<input_DEV>(ctx);
+    if (isSettingUp()) {
+        // Short-circuit DEV and DEV'
+        emitValue<output_DEVU0027>(ctx, dev);
+    }
     if (!isInputDirty<input_DO>(ctx)) return;
     uint8_t data = getValue<input_DATA>(ctx);
-    auto dev = getValue<input_DEV>(ctx);
     digitalWrite(dev->pinCS, LOW);
     shiftOut(dev->pinMOSI, dev->pinSCK, MSBFIRST, data);
     digitalWrite(dev->pinCS, HIGH);
